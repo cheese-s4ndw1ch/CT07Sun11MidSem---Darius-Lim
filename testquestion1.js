366d023820200a1cf11b83bd28869440b4a013f2
toofwess <darius.lim.rx@gmail.com>
2025-09-03 10:31:25 +0000

    3/9/2025

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,39 @@
+function setup(){
+ createCanvas(600, 400), 
+ 
+
+ 
+ 
+
+
+ball = new Sprite(ball.x, ball.y, 10) ;
+ball.x = mousex;
+ball.y = mousey;
+ball.shape = "circle";
+ball.collider = "dynamic";
+ball.mass = 2;
+ball.gravity.y = 10;
+
+
+}
+
+
+function draw() {
+ (bg, 0, 0 ,600, 400)
+ fill("green")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,40 @@
+function setup(){
+ new Canvas(600, 400), 
+ 
+
+ 
+ 
+
+
+ball = new Sprite(ball.x, ball.y, 10) ;
+ball.x = mousex;
+ball.y = mousey;
+ball.shape = "circle";
+ball.collider = "dynamic";
+ball.mass = 2;
+ball.gravity.y = 10;
+floor = new Sprite
+
+
+}
+
+
+function draw() {
+ (bg, 0, 0 ,600, 400)
+ fill("green")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
